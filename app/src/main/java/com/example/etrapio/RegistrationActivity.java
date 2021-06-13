a36fc4e5e1404b9311977129ee73e1226d485a4e
Abdul Majid <fa19-bse-014@cuilahore.edu.pk>
2021-06-13 13:32:27 +0500

    Add walkthrough Page

--- app/src/main/java/com/example/etrapio/RegistrationActivity.java
+++ app/src/main/java/com/example/etrapio/RegistrationActivity.java
@@ -3,6 +3,7 @@ package com.example.etrapio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -13,6 +14,7 @@ public class RegistrationActivity extends AppCompatActivity {
 
     EditText name, email, password;
     //private FirebaseAuth auth
+    SharedPreferences sharedPreferences;
 
 
     @Override
@@ -25,6 +27,20 @@ public class RegistrationActivity extends AppCompatActivity {
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+
+        sharedPreferences = getSharedPreferences("onBoardingScreen", MODE_PRIVATE);
+
+        boolean isFirstTime = sharedPreferences.getBoolean("firstTime", true);
+
+        if(isFirstTime){
+            SharedPreferences.Editor editor= sharedPreferences.edit();
+            editor.putBoolean("firstTime", false);
+            editor.commit();
+
+            Intent intent = new Intent(RegistrationActivity.this,OnBoardingActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     public void signup(View view){
